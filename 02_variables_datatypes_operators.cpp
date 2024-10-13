7db867c797251f71e7dfa4b37f1b0822b6c02967
Precise Goals <elitesarthakamr@gmail.com>
2024-10-13 20:10:42 +0530

    sa

---
+++ 02_variables_datatypes_operators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world";
+    // Variables
+    // Container to store the data
+
+    // Datatypes also 8 bit = 1Byte
+    // Primitive (Basic) Datatypes
+    // int, float, double, char, bool
+    //  4 ,   4  ,   8   ,  1  ,  1
+
+    // Typecasting
+    // Converting data from one type to another
+    // (IMPLICIT) conversion - data of small datatype storing in bigger datatype (easier conversion)
+    // (EXPLICIT) conversion - data of bigger datatype storing in small datatype (forced by programmer)
+
+    // cpp follows GIF function not Approximation
+    int age;
+    cin >> age;
+    cout << endl
+         << age;
+
+    // Operators 
+    // Arithmatic Operators  - add,sub,mul,div,modulon 
+    return 0;
+}
