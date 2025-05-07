4d041fc403bd8037959bbfc3f16cff628a62868a
Precise Goals <elitesarthakamr@gmail.com>
2025-05-07 23:52:30 +0530

    binary addition

--- Practice/basic/26.cpp
+++ Practice/basic/26.cpp
@@ -4,15 +4,27 @@ using namespace std;
 int main()
 {
     int b1, b2;
+    int i = 0, r = 0, sum[20];
     cout << "Enter the first Binary no: ";
     cin >> b1;
     cout << "Enter the second Binary no: ";
     cin >> b2;
 
-    while (b1 != 0 && b2 != 0) {
-        if 
-        b1 / 10;
-        b2 / 10;
+    while (b1 != 0 || b2 != 0) {
+        sum[i++] = (int)((b1 % 10 + b2 % 10 + r) % 2);
+        r = (int)((b1 % 10 + b2 % 10 + r) / 2);
+        b1 /= 10;
+        b2 /= 10;
     }
+    if (r != 0) {
+        sum[i++] += r;
+    }
+    --i;
+    cout << "The sum of two binary numbers is: ";
+    while (i >= 0) {
+        cout << (sum[i--]);
+    }
+    cout << ("\n");
+
     return 0;
 }
