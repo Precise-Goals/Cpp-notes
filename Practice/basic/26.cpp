3b0ef77dd3a64a134f21dd77512309214d717abe
Precise Goals <elitesarthakamr@gmail.com>
2025-05-07 23:41:46 +0530

    practice

---
+++ Practice/basic/26.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int b1, b2;
+    cout << "Enter the first Binary no: ";
+    cin >> b1;
+    cout << "Enter the second Binary no: ";
+    cin >> b2;
+
+    while (b1 != 0 && b2 != 0) {
+        if 
+        b1 / 10;
+        b2 / 10;
+    }
+    return 0;
+}
