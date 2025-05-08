e26746eb76f449281e37bb5a5d565d9c052fb406
Precise Goals <elitesarthakamr@gmail.com>
2025-05-09 01:35:42 +0530

    number to hour conventions

---
+++ Practice/basic/46.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n, h = 0, m = 0;
+    cout << "Enter the number: ";
+    cin >> n;
+    while (n != 0) {
+        if (n > 60) {
+            h++;
+            n -= 60;
+        } else if (n < 60) {
+            m++;
+            n -= 1;
+        }
+    }
+    cout << "Time in conventions: " << h << " : " << m << endl;
+    return 0;
+}
